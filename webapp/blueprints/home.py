de3e397274fff16548e0e4afb62c08c9d5c48e9d
masi2k203 <moto20000203@gmail.com>
2024-06-25 23:21:55 +0900

    最低限のトップページを整備

--- webapp/blueprints/home.py
+++ webapp/blueprints/home.py
@@ -1,7 +1,7 @@
 import sys
 from datetime import datetime
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 bp = Blueprint("home", __name__, static_folder="./webapp/templates/home/img")
 
@@ -11,6 +11,6 @@ def home():
     place_holder = {
         "title": "工事中",
         "now_date": str(datetime.now()),
-        "working": "Backend Info: " + str(sys.version_info),
+        "working": "Backend Info: " + str(request.headers.get("User-Agent")),
     }
     return render_template("home/index.html", **place_holder)
